73b82f887b91af684332849261b25319cb8d9950
danocorno <dbodonnell@gmail.com>
2016-10-19 20:13:43 -0500

    Add files via upload

---
+++ dealers.php
@@ -0,0 +1,24 @@
+<?php
+include 'inc/header1.php';
+?>
+<title>ADA Hold 'Em | Texas Hold 'Em Dealers</title>
+<meta property="og:title" content="ADA Hold 'Em | Texas Hold 'Em Dealers">
+<?php
+include 'inc/header2.php';
+include 'inc/body.php';
+?>
+
+<iframe id="calendardealer" src="https://www.google.com/calendar/embed?showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=ohnthugv93u5eqk0680g1emvh0%40group.calendar.google.com&amp;color=%2328754E&amp;ctz=America%2FChicago" style=" border-width:0; " width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+
+            <!-- FOOTER -->
+            <section class="hidden-lg hidden-md" id="footer">
+                <div class="row nomargin">
+                    <a id="scrollTop" href="#content"><p class="wow fadeIn" data-wow-delay="0.25s" data-wow-duration="0.75s"><i class="fa fa-hand-o-up fa-fw fa-lg"></i>Back to top</p></a>
+                </div>
+            </section>
+
+
+<?php
+include 'inc/menu.php';
+include 'inc/footer.php';
+?>
